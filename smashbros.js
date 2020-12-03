ced99127a0fa36a91a303afe67a0449f69a8ba70
Michael Andrew Moore <74065788+MikeyAMoore@users.noreply.github.com>
2020-12-02 20:40:57 -0700

    First Time coding with java.

---
+++ smashbros.js
@@ -0,0 +1,35 @@
+db.SuperSmashBros.insert({
+    "name": "Link",
+    "game": "Legend of Zelda",
+    "weapon": "Sword"
+})
+
+db.SuperSmashBros.insert({
+    "name": "Mario",
+    "game": "Super Mario Bros",
+    "weapon": "Stomp"
+})
+
+db.SuperSmashBros.insert({
+    "name": "Samus",
+    "game": "Metroid",
+    "weapon": "Pulse Canon"
+})
+
+db.SuperSmashBros.insert({
+    "name": "Fox",
+    "game": "Star Fox",
+    "weapon": "Blaster Pistol"
+})
+
+db.SuperSmashBros.insert({
+    "name": "Cpt. Falcon",
+    "game": "F-Zero",
+    "weapon": "Falcon Punch"
+})
+
+db.SuperSmashBros.insert({
+    "name": "Donkey kong",
+    "game": "Donkey Kong",
+    "weapon": "Power Smash"
+})
